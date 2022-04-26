e10b0c40a2ada4015c898a4d951c0547be170adc
Naresh Bhatia <naresh.archfirst@gmail.com>
2022-04-25 21:06:17 -0400

    Minor changes to plop-generators

--- plop-generators/apps/movie-magic/src/components/Header/Header.tsx
+++ plop-generators/apps/movie-magic/src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import { CogIcon } from '@heroicons/react/outline';
 import { useNavigate } from 'react-router-dom';
 
--- plop-generators/apps/movie-magic/src/components/PageLayout/PageLayout.tsx
+++ plop-generators/apps/movie-magic/src/components/PageLayout/PageLayout.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import { Outlet } from 'react-router-dom';
 import { Header } from '../Header';
 
