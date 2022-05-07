0dce06297209feac76cf4f5879756a0465f0f84a
Naresh Bhatia <naresh.archfirst@gmail.com>
2022-05-07 11:06:11 -0400

    Refined tsconfig for plop-generators

--- plop-generators/apps/movie-magic/src/App.tsx
+++ plop-generators/apps/movie-magic/src/App.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { PageLayout } from './components';
 import { HomePage, NotFoundPage, SettingsPage } from './pages';
--- plop-generators/apps/movie-magic/src/pages/HomePage/HomePage.tsx
+++ plop-generators/apps/movie-magic/src/pages/HomePage/HomePage.tsx
@@ -1,8 +1,11 @@
+import { Button } from 'ui-lib';
+
 export function HomePage() {
   return (
     <div className="p-3">
       <div className="card p-2">
         <h2 className="h5 line-height-none mb-2">Top 10 Movies Of All Time</h2>
+        <Button>Buy</Button>
       </div>
     </div>
   );
