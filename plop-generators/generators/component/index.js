fd3a09576bb3265a72aa848b486e4544c7baac63
Naresh Bhatia <naresh.archfirst@gmail.com>
2022-05-08 19:01:11 -0400

    Replaced fuzzypath with list and custom list of workspace choices

--- plop-generators/generators/component/index.js
+++ plop-generators/generators/component/index.js
@@ -1,3 +1,6 @@
+const treeWalk = require('klaw-sync');
+const path = require('path');
+
 module.exports = {
   description: 'Component Generator',
   prompts: [
@@ -6,24 +9,36 @@ module.exports = {
       name: 'name',
       message: 'Component name',
     },
+    // {
+    //   type: 'fuzzypath',
+    //   name: 'workspace',
+    //   itemType: 'directory',
+    //   message: 'Which workspace should this go to?',
+    //   excludePath: (nodePath) => {
+    //     // If a parent directory is excluded then children will not be
+    //     // traversed. Hence, '.' must be included.
+    //     const includePath =
+    //       nodePath.startsWith('.') ||
+    //       nodePath.startsWith('apps') ||
+    //       nodePath.startsWith('packages');
+    //     return !includePath;
+    //   },
+    //   depthLimit: 1,
+    // },
     {
-      type: 'fuzzypath',
+      type: 'list',
       name: 'workspace',
-      itemType: 'directory',
       message: 'Which workspace should this go to?',
-      excludePath: (nodePath) => {
-        // If a parent directory is excluded then children will not be
-        // traversed. Hence, '.' must be included.
-        const includePath =
-          nodePath.startsWith('.') ||
-          nodePath.startsWith('apps') ||
-          nodePath.startsWith('packages');
-        return !includePath;
+      choices: () => {
+        const dirSpecs = resolvePaths(process.cwd(), ['apps/*', 'packages/*']);
+        return dirSpecs.map((dirSpec) => ({
+          name: dirSpec.name,
+          value: dirSpec.path,
+        }));
       },
-      depthLimit: 1,
     },
   ],
-  actions: ({ name, folder }) => {
+  actions: ({ name, workspace }) => {
     return [
       {
         type: 'addMany',
@@ -40,3 +55,30 @@ module.exports = {
     ];
   },
 };
+
+function resolvePaths(rootDir, relativePaths) {
+  const specs = [];
+
+  relativePaths.forEach((relativePath) => {
+    const isGlob = relativePath.endsWith('*');
+    if (isGlob) {
+      // strip the * at the end
+      const relativeParentDir = relativePath.slice(0, -1);
+      const parentDir = path.join(rootDir, relativeParentDir);
+      const childDirs = treeWalk(parentDir, { nofile: true, depthLimit: 0 });
+      childDirs.forEach((childDir) => {
+        specs.push({
+          name: path.relative(rootDir, childDir.path),
+          path: childDir.path,
+        });
+      });
+    } else {
+      specs.push({
+        name: relativePath,
+        path: path.join(rootDir, relativePath),
+      });
+    }
+  });
+
+  return specs;
+}
